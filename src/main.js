b85482055d1527241f1557baedf264d67b512696
marshal021 <2532637185@qq.com>
2018-08-15 19:37:10 +0800

    添加测试数据

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import store from './store'
 import VueSweetalert2 from './plugins/vue-sweetalert2'
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 Vue.use(VueSweetalert2)
 
@@ -16,7 +18,27 @@ Vue.use(Message)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
+
 new Vue({
   el: '#app',
   router,
